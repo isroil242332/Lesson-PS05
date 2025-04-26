e148b98eb7e1ce6355f5f95fa4df3e2e9baf35e5
isroil242332 <isroil-islam@mail.ru>
2025-04-26 15:31:16 +0300

    add csv code and file

--- divanpars/divanpars/spiders/divannewpars.py
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -1,6 +1,7 @@
 import scrapy
+import csv
 urls = 'https://www.divan.ru/category/svet'
-
+parsed_data = []
 class DivannewparsSpider(scrapy.Spider):
     name = "divannewpars"
     allowed_domains = ["https://divan.ru"]
@@ -9,8 +10,21 @@ class DivannewparsSpider(scrapy.Spider):
     def parse(self, response):
         lightings = response.css("div._Ud0k")
         for light in lightings:
-            yield {
+
+            pars3 = {
                 "name" : light.css("div.lsooF span::text").get(),
                 "price" : light.css("div.q5Uds span::text").get(),
                 "url" : urls + light.css("a").attrib["href"]
             }
+
+
+            yield pars3
+            name3 = pars3.get("name")
+            price3 = pars3.get("price")
+            url3 = pars3.get("url")
+            parsed_data.append([name3, price3, url3])
+
+        with open("svet.csv", 'w',newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Название', 'Цена', 'Ссилка'])
+            writer.writerows(parsed_data)
